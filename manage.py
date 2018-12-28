fe6d9c0cd1502606d8b81e86a9a4c89ec712fa15
程航 <chengh@cyberpeace.cn>
2018-12-28 23:07:16 +0800

    demo

---
+++ manage.py
@@ -0,0 +1,11 @@
+from flask_migrate import MigrateCommand
+from flask_script import Manager
+
+from AD import create_app
+
+app = create_app('develop')
+manage = Manager(app=app)
+manage.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manage.run()
